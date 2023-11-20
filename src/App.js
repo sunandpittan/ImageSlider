8d123e029fe5b78b3e1d5a6ebd2a0f33a4397f3a
sunandpittan <sunandpittan@gmail.com>
2023-11-20 16:47:09 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Fragment } from "react";
+import Slider from "./Slider";
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Slider />
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Slider.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  {
+    url: "https://lh5.googleusercontent.com/p/AF1QipMgwTINZonkSD7SStQYLN0v-CdGFj8XyKVEpaUz=w810-h468-n-k-no",
+  },
+  {
+    url: "https://lh5.googleusercontent.com/p/AF1QipN5gz_v-wuTGZrN-vPwEmpi5fSG5VxoWfMaNWAo=w810-h468-n-k-no",
+  },
+  {
+    url: "https://lh5.googleusercontent.com/p/AF1QipMTFXa5GXoGnLvxEeCcghb60oQ7EnreqgR9ACMl=w810-h468-n-k-no",
+  },
+  {
+    url: "https://lh5.googleusercontent.com/p/AF1QipNpT3t7Lx61s4C1BH4pWlw1-oe9-_95pvwe4wjb=w810-h468-n-k-no",
+  },
+];
+export default function Slider() {
+  const [transspeed, setTransspeed] = useState(0.5);
+
+  return (
+    <div className="w-75 mx-auto mt-2 shadow">
+      <SimpleImageSlider
+        width={"75%"}
+        height={"80vh"}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        transSpeed={transspeed}
+        style={{ width: "75%", height: "80vh" }}
+      />
+      <div className="w-50 mx-auto">
+        <Form.Label>Transition speed:</Form.Label>
+        <Form.Range
+          value={transspeed}
+          max={10}
+          onChange={(e) => setTransspeed(e.target.value)}
+        />
+        <Form.Label>{transspeed}</Form.Label>
+      </div>
+    </div>
+  );
+}
